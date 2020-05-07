65349df599920ba2d0ff99aff8d7dd37e8f8a2c6
Iskander Dosgali <idosgali@gmail.com>
2020-05-07 23:01:48 +0600

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+Auth::routes([
+    'reset' => false,
+    'confirm' => false,
+    'verify' => false,
+]);
+
+
+
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'MainController@index')->name('index');
+Route::get('/categories', 'MainController@categories')->name('categories');
+
+Route::get('/basket', 'BasketController@basket')->name('basket');
+Route::get('/basket/place', 'BasketController@basketPlace')->name('basket-place');
+Route::post('/basket/add/{id}','BasketController@basketAdd')->name('basket-add');
+Route::post('/basket/remove/{id}','BasketController@basketRemove')->name('basket-remove');
+Route::post('/basket/place', 'BasketController@basketConfirm')->name('basket-confirm');
+
+Route::get('/{category}', 'MainController@category')->name('category');
+Route::get('/{category}/{product?}', 'MainController@product')->name('product');
+
+
+
+
+
