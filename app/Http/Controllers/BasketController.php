4b5ca6ac269bf62e976be5018d06365abd601327
Iskander Dosgali <idosgali@gmail.com>
2020-05-08 18:53:59 +0600

    initial commit 3

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Order;
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BasketController extends Controller
 {
@@ -59,6 +60,12 @@ class BasketController extends Controller
         else {
             $order->products()->attach($productId);
         }
+
+        if (Auth::check()){
+            $order->user_id = Auth::id();
+            $order->save();
+        }
+
 
         $product = Product::find($productId);
         session()->flash('success', 'Добавлен товар', $product->name);
